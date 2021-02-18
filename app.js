55cb2d7db57e280588ade6d59ec1f7520a720d35
cjsellwood <c.sellwood22@gmail.com>
2021-02-19 10:58:17 +1100

    Changed cookie config g for heroku

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ const sessionConfig = {
   resave: false,
   saveUninitialized: true,
   cookie: {
-    httpOnly: true,
+    // httpOnly: true,
+    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   }
